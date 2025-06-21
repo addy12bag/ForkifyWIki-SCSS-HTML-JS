c867ab7f89a57bf72be2de252744af44f98942c3
Sayantan Bag <98514700+addy12bag@users.noreply.github.com>
2025-06-21 18:40:23 +0530

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1a2ad8ac-fdf4-4bdf-a86e-1bdafd53b505';
+export const MODAL_CLOSE_SEC = 2.5;
